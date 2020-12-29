b779b4a1a7ab86547e681ac14ae780f713958bd1
ruucm <ruucm.a@gmail.com>
2020-12-30 08:58:47 +0900

    add Wrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./styles.css";
+import styled from "styled-components";
+
+const Wrap = styled.div`
+  background: green;
+`;
 
 export default function App() {
   return (
-    <div className="App">
+    <Wrap>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-    </div>
+    </Wrap>
   );
 }
