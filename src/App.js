00c5a467ac78cfdcca992d30ae40956bb7ac36c0
ruucm <ruucm.a@gmail.com>
2020-12-30 09:25:55 +0900

    Content added

--- src/App.js
+++ src/App.js
@@ -2,32 +2,59 @@ import React from "react";
 import "./styles.css";
 import styled from "styled-components";
 
-const Row = styled.div`
+const Guide = styled.div`
+  background: blue;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: -1;
+`;
+
+const GuideRow = styled.div`
   background: green;
   display: grid;
   grid-template-columns: repeat(12, 1fr);
   column-gap: 10px;
+  margin: 0px 20px;
 `;
-const Column = styled.div`
+const GuideColumn = styled.div`
   background: pink;
   height: 100vh;
 `;
 
+const Content = styled.div``;
+
+const Row = styled.div`
+  background: yellowgreen;
+  margin: 0px 20px;
+`;
+const Column = styled.div``;
+
 export default function App() {
   return (
-    <Row>
-      <Column>Column</Column>
-      <Column>Column</Column>
-      <Column>Column</Column>
-      <Column>Column</Column>
-      <Column>Column</Column>
-      <Column>Column</Column>
-      <Column>Column</Column>
-      <Column>Column</Column>
-      <Column>Column</Column>
-      <Column>Column</Column>
-      <Column>Column</Column>
-      <Column>Column</Column>
-    </Row>
+    <>
+      <Guide>
+        <GuideRow>
+          <GuideColumn />
+          <GuideColumn />
+          <GuideColumn />
+          <GuideColumn />
+          <GuideColumn />
+          <GuideColumn />
+          <GuideColumn />
+          <GuideColumn />
+          <GuideColumn />
+          <GuideColumn />
+          <GuideColumn />
+          <GuideColumn />
+        </GuideRow>
+      </Guide>
+      <Content>
+        <Row>
+          <Column>Column</Column>
+          <Column>Column</Column>
+        </Row>
+      </Content>
+    </>
   );
 }
