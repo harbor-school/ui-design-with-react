1eb14b217d9c10327c65bce8fc2e1cffdcfc12ae
ruucm <ruucm.a@gmail.com>
2020-12-30 09:10:23 +0900

    Add Row & Column

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import React from "react";
 import "./styles.css";
 import styled from "styled-components";
 
-const Wrap = styled.div`
+const Row = styled.div`
   background: green;
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  column-gap: 10px;
+`;
+const Column = styled.div`
+  background: pink;
+  height: 100vh;
 `;
 
 export default function App() {
   return (
-    <Wrap>
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </Wrap>
+    <Row>
+      <Column>Column</Column>
+      <Column>Column</Column>
+      <Column>Column</Column>
+      <Column>Column</Column>
+      <Column>Column</Column>
+      <Column>Column</Column>
+      <Column>Column</Column>
+      <Column>Column</Column>
+      <Column>Column</Column>
+      <Column>Column</Column>
+      <Column>Column</Column>
+      <Column>Column</Column>
+    </Row>
   );
 }
