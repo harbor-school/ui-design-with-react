472e224c83c9f621c9edbf7d24e53c7b4768abb2
ruucm <ruucm.a@gmail.com>
2020-12-31 16:36:45 +0900

    add font files (Albra)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-import "./styles.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Home, Profile, MyClubs, Settings } from "./pages";
 import { Guide, Header, Footer } from "./components";
 import { colors } from "./constants";
 import styled from "styled-components";
+import "./styles.css";
+// import "./assets/fonts/Albra/stylesheet.css";
 
 const Wrap = styled.div`
   /* font-family: "Playfair Display", serif; */
-  font-family: "Albra", serif;
+  font-family: "AlbraBold";
   background: ${colors.bacgkroundPrimary};
 `;
 
--- src/assets/illusts/User.js
+++ src/assets/illusts/User.js
@@ -17,20 +17,20 @@ export function User(props) {
       <path
         d="M 20 21 L 20 19 C 20 16.791 18.209 15 16 15 L 8 15 C 5.791 15 4 16.791 4 19 L 4 21"
         fill="transparent"
-        stroke-width="2"
+        strokeWidth="2"
         stroke="white"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-dasharray=""
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeDasharray=""
       ></path>
       <path
         d="M 8 7 C 8 4.791 9.791 3 12 3 C 14.209 3 16 4.791 16 7 C 16 9.209 14.209 11 12 11 C 9.791 11 8 9.209 8 7 Z"
         fill="transparent"
-        stroke-width="2"
+        strokeWidth="2"
         stroke="white"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-dasharray=""
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeDasharray=""
       ></path>
     </Illust>
   );
