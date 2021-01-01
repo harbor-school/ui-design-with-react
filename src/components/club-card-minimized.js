91c6f71a842ff74460cb01cc973030d25badf5e1
ruucm <ruucm.a@gmail.com>
2021-01-01 11:35:10 +0900

    update styles

--- src/components/club-card-minimized.js
+++ src/components/club-card-minimized.js
@@ -7,19 +7,24 @@ import { Link } from "react-router-dom";
 const Info = styled(Link)`
   display: flex;
   justify-content: space-between;
-  padding: 12px 15px;
+  padding: 20px;
   align-items: center;
   background: ${colors.bacgkroundSecondary};
   border-radius: 6px;
   text-decoration: none;
   color: inherit;
+  max-width: 250px;
+  letter-spacing: 0.2px;
+  /* font-family: "Noto Sans KR", sans-serif; */
 `;
-const Title = styled.h3``;
+const Title = styled.h3`
+  font-size: 20px;
+`;
 
 export function ClubCardMinimized({ title = "Three.js Club" }) {
   return (
     <Info to="/club/threejs-club">
-      <Illust.House height="28px" />
+      <Illust.House height="24px" />
       <Title>{title}</Title>
     </Info>
   );
